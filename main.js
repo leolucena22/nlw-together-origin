4abd111b4d3080d0841e53d0f27068fcc287acb2
leolucena22 <leonardolucena44@gmail.com>
2021-06-21 22:48:17 -0300

    first commit - 02

---
+++ main.js
@@ -0,0 +1,32 @@
+// Abre e fecha o menu quando clicca
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// Quando clicar em algum link do menu, fechá-lo
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// Mudar o header da página quando der scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // scroll é menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
