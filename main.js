05b6dd5b73c8396a135e2bb87cfde6838a5fa65e
leolucena22 <leonardolucena44@gmail.com>
2021-06-22 22:04:21 -0300

    Aula 03

--- main.js
+++ main.js
@@ -30,3 +30,31 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+// Testimonial carrossel slider swiperjs
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// ScrollReveal: Mostra elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  durantion: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
